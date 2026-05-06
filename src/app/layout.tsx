053d71db51049bacccc473b4b48b60b968b95e93
Aniruddha khandait <samyakkhandait@gmail.com>
2026-05-06 12:02:47 +0530

    initail commmit - install all the dependecies as well as connection with neon db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+ <ClerkProvider>
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark `}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+</ClerkProvider>
   );
 }
